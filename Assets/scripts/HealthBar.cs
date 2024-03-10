d7b6df52b349cab01876494e435924c558bece9a
satrya070 <satrya@spotr.ai>
2024-03-10 20:59:03 +0100

    added dealthbar for enemy and divided canvas

--- Assets/scripts/HealthBar.cs
+++ Assets/scripts/HealthBar.cs
@@ -5,25 +5,19 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider healthBar;
-    //int PlayerCurrentHealth;
+    Slider healthBar;
+    GameObject Owner;
+    Health OwnerHealth;
 
-
-    [SerializeField]
-    PlayerSpaceship Player;
-    Health PlayerHealth;
-
-    // Start is called before the first frame update
     void Start()
     {
-        PlayerHealth = Player.GetComponent<Health>();  //GameObject.Find("PlayerSpaceShip").GetComponent<Health>();
-        //PlayerCurrentHealth = PlayerHealth.currentHealth; //GameObject.Find("PlayerSpaceShip").GetComponent<Health>().currentHealth;
-        //healthBar = healthBar.GetComponent<Slider>();
-        //healthBar.value = PlayerHealth - 50; 
+        healthBar = GetComponent<Slider>();
+        Owner = transform.root.gameObject;
+        OwnerHealth = Owner.GetComponent<Health>();
     }
 
     void Update()
     {
-        healthBar.value = PlayerHealth.currentHealth;
+        healthBar.value = OwnerHealth.currentHealth;
     }
 }
