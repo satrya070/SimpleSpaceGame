c5891f41105a9f3434b30bcb52e7d410822a3c05
satrya070 <satrya@spotr.ai>
2024-03-12 20:31:44 +0100

    added gamemanger to all levels and working menu items

---
+++ Assets/scripts/PauseManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+
+        void PauseGame()
+        {
+            Time.timeScale = 0f;
+            isPaused = true;
+            //pauseMenu.SetActive(true);
+        }
+
+        void ResumeGame()
+        {
+            Time.timeScale = 1f;
+            isPaused = false;
+            //pauseMenu.SetActive(false);
+        }
+    }
+}
