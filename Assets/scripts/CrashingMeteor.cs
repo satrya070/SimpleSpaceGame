9729ea5e7bb57dc034feaca63a7f687e8f818173
satrya070 <satrya@spotr.ai>
2024-03-17 14:02:56 +0100

    on meteor impact add damage and explosion

--- Assets/scripts/CrashingMeteor.cs
+++ Assets/scripts/CrashingMeteor.cs
@@ -7,11 +7,18 @@ public class CrashingMeteor : MonoBehaviour
     public Transform crashPoint;
     public float crashSpeed = 100f;
     Rigidbody rb;
+    Damage damage;
+
+    [SerializeField]
+    GameObject explosion;
+
+    List<string> TargetTags = new List<string>() {"Player", "SpaceStation" };
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        damage = GetComponent<Damage>();
     }
 
     // Update is called once per frame
@@ -20,8 +27,24 @@ public class CrashingMeteor : MonoBehaviour
         Vector3 moveDirection = (crashPoint.position - transform.position).normalized;
         if(crashPoint)
         {
-            Debug.Log("Moving rock!");
+            //Debug.Log("Moving rock!");
             rb.AddForce(moveDirection * crashSpeed);
+        }
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if (TargetTags.Contains(other.gameObject.tag))
+        {
+            Debug.Log(other.gameObject.name);
+            CombatHandler.ApplyDamage(other.gameObject, damage);
+
+            if (explosion)
+            {
+                GameObject explode = Instantiate(explosion, transform.position, transform.rotation);//.parent.transform);
+                Destroy(explode, 1f);
+            }
+
+            Destroy(gameObject);
         }
     }
 }
