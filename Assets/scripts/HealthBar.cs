3442e7549c4bc40d1efb0d1c7e9d88f0287df320
satrya070 <satrya@spotr.ai>
2024-03-26 20:56:02 +0100

    enemy healtbars faces player

--- Assets/scripts/HealthBar.cs
+++ Assets/scripts/HealthBar.cs
@@ -8,16 +8,30 @@ public class HealthBar : MonoBehaviour
     Slider healthBar;
     GameObject Owner;
     Health OwnerHealth;
+    GameObject MainCameraTarget;
 
     void Start()
     {
         healthBar = GetComponent<Slider>();
         Owner = transform.root.gameObject;
         OwnerHealth = Owner.GetComponent<Health>();
+
+        if(gameObject.transform.root.gameObject.tag != "Player")
+        {
+            Debug.Log($"{gameObject.transform.root.transform.gameObject.name} is searhcing for player camera...");
+            MainCameraTarget = GameObject.FindWithTag("MainCamera");
+            Debug.Log(MainCameraTarget.transform.position);
+        }
     }
 
     void Update()
     {
         healthBar.value = OwnerHealth.currentHealth;
+
+        if(MainCameraTarget)
+        {
+            //Debug.Log(transform.parent.transform.name);
+            transform.parent.transform.LookAt(MainCameraTarget.transform.position);
+        }
     }
 }
