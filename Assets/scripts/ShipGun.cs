0dbdfda7a8116945b535a281698e243f9234b585
satrya070 <satrya@spotr.ai>
2024-05-06 17:40:29 +0200

    latest laser update

--- Assets/scripts/ShipGun.cs
+++ Assets/scripts/ShipGun.cs
@@ -15,6 +15,7 @@ public class ShipGun : MonoBehaviour
 
     RectTransform crosshair;
     Camera mainCamera;
+    Vector3 CrosshairSceenPos = new Vector3((Screen.width / 2), (Screen.height / 2 + 19.1f), 1f);
 
     private void Start()
     {
@@ -47,29 +48,47 @@ public class ShipGun : MonoBehaviour
     void shootRay()
     {
         AudioManager.Instance.PlaySfx("LaserGun");
-        Ray ray = new Ray(transform.position, transform.forward * range);
 
-        Debug.DrawRay(transform.position, ray.direction * 1000, Color.green, 10f);
+        // direction vector from camera through crosshair
+        Vector3 DirectionVector = mainCamera.ScreenToWorldPoint(CrosshairSceenPos) - mainCamera.transform.position;
+        
+        // constructs the ray to be cast from camera through crossheir in world space
+        Ray crossRay = new Ray(mainCamera.transform.position, DirectionVector * range);
+
+        // default laser target if no raycast (ray through crossheir * range 1000f)
+        Vector3 laserTarget = mainCamera.transform.position + (DirectionVector * range);
+        Debug.DrawRay(mainCamera.transform.position, DirectionVector * range, Color.green, 10f);
+
+        // cast the ray and set laser target if it has a hit
+        RaycastHit crossHit;
+        bool crossHasHit = Physics.Raycast(crossRay, out crossHit);
+        if(crossHasHit)
+        {
+            // if ray crossheir has hit send laser to point of hit instead
+            laserTarget = crossHit.point;
+            //Debug.Log($"Cross has a hit!: {crossHit.point}");
+        }
 
         GameObject laser = GameObject.Instantiate(m_shotPrefab, transform.position, transform.rotation) as GameObject;
         laser.GetComponent<ShotBehavior>().setTargetComponents(
             shipOwner.gameObject.tag,
-            transform.position + (transform.forward * range),
+            laserTarget,//transform.position + (transform.forward * range),
             shipOwner.GetComponent<Damage>(),
             shipOwner.gameObject.tag
         );
 
+        // TODO delete if no issues
         // draw a line throught the bottom right frustum (adds/subtracts 1f) for 0 start index
         //Debug.DrawLine(mainCamera.transform.position, mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth -1, 1f,1f)), Color.red, 5f, false);
 
-        Vector3 DirectionVector = mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth -1, 1f,1f)) - mainCamera.transform.position; 
+        //Vector3 DirectionVector = mainCamera.ScreenToWorldPoint(new Vector3(mainCamera.pixelWidth -1, 1f,1f)) - mainCamera.transform.position; 
         //Debug.DrawLine(mainCamera.transform.position, mainCamera.transform.position + (DirectionVector * 100f), Color.red, 5f, false);
 
         // get middle of screen then adds crosshair y-offset
-        Vector3 SceenCenter = new Vector3((Screen.width / 2), (Screen.height / 2 + 19.1f), 1f);
-        // convert that screenpoint to a world point & get the direction vector relative to camera position
-        Vector3 DirectionVector2 = mainCamera.ScreenToWorldPoint(SceenCenter) - mainCamera.transform.position;
-        Debug.DrawLine(mainCamera.transform.position, mainCamera.transform.position + (DirectionVector2 * 200f), Color.red, 5f, true);
+        // Vector3 SceenCenter = new Vector3((Screen.width / 2), (Screen.height / 2 + 19.1f), 1f);
+        // // convert that screenpoint to a world point & get the direction vector relative to camera position
+        // Vector3 DirectionVector2 = mainCamera.ScreenToWorldPoint(SceenCenter) - mainCamera.transform.position;
+        // Debug.DrawLine(mainCamera.transform.position, mainCamera.transform.position + (DirectionVector2 * 300f), Color.red, 5f, true);
 
     }
 }
